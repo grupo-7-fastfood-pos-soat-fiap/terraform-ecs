94707af833506e03389f8d3fd421a6716d09dfc7
analuisabavati <ana_bavati@hotmail.com>
2023-11-05 17:08:07 -0300

    Adding db-setup

--- env/prod/Main.tf
+++ env/prod/Main.tf
@@ -3,9 +3,9 @@ module "prod" {
 
   cargo_IAM    = "prod"
   ambiente     = "prod"
-  rds_db_name  = "db_name"
-  rds_username = "username"
-  rds_password = "password"
+  rds_db_name  = "postgres"
+  rds_username = "postgres"
+  rds_password = "#F1apFastF00d"
 }
 
 terraform {
--- infra/GrupoSeguranca.tf
+++ infra/GrupoSeguranca.tf
@@ -54,7 +54,7 @@ resource "aws_security_group" "rds" {
     protocol        = "tcp"
     from_port       = "5432"
     to_port         = "5432"
-    security_groups = [aws_security_group.privado.id]
+    security_groups = [aws_security_group.alb.id]
   }
 
   egress {
