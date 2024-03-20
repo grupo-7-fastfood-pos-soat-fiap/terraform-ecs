4f3987fe11c6821f1a432a28958dafd46249c437
analuisabavati <ana_bavati@hotmail.com>
2024-03-20 11:09:42 -0300

    Reutilizando infra para o hackathon

--- env/prod/Main.tf
+++ env/prod/Main.tf
@@ -2,13 +2,13 @@ module "prod" {
   source = "../../infra"
 
   cargo_IAM = "prod"
-  ambiente = "prod"
+  ambiente  = "prod"
 }
 
 terraform {
   backend "remote" {
     organization = "fiap-postech-groupo7"
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
 
     workspaces {
       prefix = "hackathon"
